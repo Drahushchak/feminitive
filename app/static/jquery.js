7bf95db117de1becd57e40ad6c421d78e3f8cca2
Drahushchak <dragushhakigor@gmail.com>
2020-12-02 03:08:01 +0200

    fixed PWA installation button not appearing

---
+++ app/static/jquery.js
@@ -0,0 +1,21 @@
+$('input[id=upload-file]').change(function(file) {
+    $('#')
+});
+
+$('input.source-checkbox').change(function(el){
+        let n_selected = $('input.source-checkbox:checked').length
+        let submit_b = $('#source-dashboard-submit')
+        if(n_selected==0){
+            submit_b.addClass('d-none')
+            submit_b.removeClass('d-block')
+        } else if(n_selected==1){
+            submit_b.removeClass('d-none')
+            submit_b.addClass('d-block')
+            submit_b.val('Show Chart')
+        } else {
+            submit_b.removeClass('d-none')
+            submit_b.addClass('d-block')
+            submit_b.val('Show Comparative Chart')
+        }
+    }
+)
